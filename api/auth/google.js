6c0b14bc5a99b11708e62a48b61c773b31aa7965
Diomar Gonçalves <50972479+DiomarGoncalves@users.noreply.github.com>
2025-04-16 21:56:43 -0300

    update

--- api/auth/google.js
+++ api/auth/google.js
@@ -1,8 +1,6 @@
 const { google } = require("googleapis");
 const express = require("express");
-const app = express();
-
-app.use(express.json());
+const router = express.Router();
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
@@ -10,13 +8,14 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_REDIRECT_URI
 );
 
-app.post("/api/auth/google", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { credential } = req.body;
     const ticket = await oauth2Client.verifyIdToken({
       idToken: credential,
       audience: process.env.GOOGLE_CLIENT_ID,
     });
+
     const payload = ticket.getPayload();
     const userId = payload["sub"];
     const email = payload["email"];
@@ -24,10 +23,15 @@ app.post("/api/auth/google", async (req, res) => {
 
     // Aqui você pode salvar o usuário no banco de dados, se necessário.
 
-    res.status(200).json({ token: `fake-jwt-token-for-${userId}`, email, name });
+    res.status(200).json({
+      token: `fake-jwt-token-for-${userId}`,
+      email,
+      name,
+    });
   } catch (error) {
+    console.error("Erro ao autenticar com Google:", error);
     res.status(400).json({ error: "Autenticação falhou" });
   }
 });
 
-module.exports = app;
+module.exports = router;
